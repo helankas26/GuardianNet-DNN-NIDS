d60985e51c4d8cddf154d25481e591b5e7112b4b
Helanka Singhapurage <helankas26@gmail.com>
2024-01-03 08:32:18 +0530

    add login and signup controller

---
+++ src/guardiannet/app/controller/login.py
@@ -0,0 +1,42 @@
+from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtWidgets import QWidget, QMessageBox
+
+from src.guardiannet.app.model import Users
+from src.guardiannet.app.util import PasswordManager, MongoDBManager
+from src.guardiannet.app.view import Ui_loginForm
+
+
+class Login(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_loginForm()
+        self.ui.setupUi(self)
+        self.__connectSignalsAndSlots()
+
+        self.show()
+
+    def __connectSignalsAndSlots(self):
+        self.ui.btnLogin.clicked.connect(self.__login)
+
+    @pyqtSlot()
+    def __login(self):
+        username = self.ui.txtUsername.text()
+        password = self.ui.txtPassword.text()
+
+        if username == '' or password == '':
+            QMessageBox.warning(self, 'Error', 'Please fill out all the fields!')
+            return
+
+        try:
+            MongoDBManager()
+            user = Users.objects(username=username).first()
+            if PasswordManager.verifyPassword(password, user.password):
+                print("login")
+            else:
+                QMessageBox.warning(self, 'Warning', 'Incorrect password!')
+        except AttributeError as e:
+            QMessageBox.warning(self, 'Warning', 'No user found!')
+            print(f"Caught an exception: {type(e).__name__}: {e}")
+        except Exception as e:
+            print(f"Caught an exception: {type(e).__name__}: {e}")
---
+++ src/guardiannet/app/controller/signup.py
@@ -0,0 +1,46 @@
+from PyQt6.QtWidgets import QWidget, QMessageBox
+from src.guardiannet.app.config import SystemInitializer
+from src.guardiannet.app.controller import Login
+from src.guardiannet.app.util import WindowManager
+from src.guardiannet.app.view import Ui_signupForm
+from PyQt6.QtCore import pyqtSlot
+
+
+class Signup(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_signupForm()
+        self.ui.setupUi(self)
+        self.__connectSignalsAndSlots()
+
+        self.show()
+
+    def __connectSignalsAndSlots(self):
+        self.ui.btnSignUp.clicked.connect(self.__signup)
+
+    @pyqtSlot()
+    def __signup(self):
+        username = self.ui.txtUsername.text()
+        password = self.ui.txtPassword.text()
+        confirm_password = self.ui.txtConfirmPassword.text()
+
+        if username == '' or password == '' or confirm_password == '':
+            QMessageBox.warning(self, 'Error', 'Please fill out all the fields!')
+            return
+
+        if password != confirm_password:
+            QMessageBox.warning(self, 'Error', 'Password does not match!')
+            return
+
+        answer = QMessageBox.question(self, 'Confirmation', 'Do you want to create account?',
+                                      QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+
+        if answer == QMessageBox.StandardButton.Yes:
+            if SystemInitializer.initialize_system(username, password):
+                WindowManager.window = Login()
+            else:
+                QMessageBox.warning(self, 'Error', 'Error initializing system!')
+                return
+
+        return
