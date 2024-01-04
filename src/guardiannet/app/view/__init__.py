05d0427a996cbf79bced9bc3b0ad0dc667c93eb6
Helanka Singhapurage <helankas26@gmail.com>
2024-01-05 00:04:40 +0530

    add setting view

--- src/guardiannet/app/view/__init__.py
+++ src/guardiannet/app/view/__init__.py
@@ -2,3 +2,4 @@ from .signup_form import Ui_signupForm
 from .login_form import Ui_loginForm
 from .main_window import Ui_mainWindow
 from .status_form import Ui_statusForm
+from .settings_form import Ui_settingsForm
