649641bfdb378b495530be4684b43706b01c2232
Helanka Singhapurage <helankas26@gmail.com>
2024-01-05 16:05:31 +0530

    change return type

--- src/guardiannet/app/util/password_manager.py
+++ src/guardiannet/app/util/password_manager.py
@@ -4,7 +4,7 @@ import bcrypt
 class PasswordManager:
     @staticmethod
     def hashPassword(password):
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     @staticmethod
     def verifyPassword(password, hashed_password):
