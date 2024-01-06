85eed5ade52c52609a4ad69019982b33ed83969b
Helanka Singhapurage <helankas26@gmail.com>
2024-01-07 01:12:23 +0530

    add scan view

--- src/guardiannet/app/view/__init__.py
+++ src/guardiannet/app/view/__init__.py
@@ -3,3 +3,4 @@ from .login_form import Ui_loginForm
 from .main_window import Ui_mainWindow
 from .status_form import Ui_statusForm
 from .settings_form import Ui_settingsForm
+from .scan_form import Ui_scanForm
