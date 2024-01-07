0364572b7db5806f701a35e2b3d4bfa6a7f6f137
Helanka Singhapurage <helankas26@gmail.com>
2024-01-07 23:32:41 +0530

    add history and history item view

--- src/guardiannet/app/view/__init__.py
+++ src/guardiannet/app/view/__init__.py
@@ -4,3 +4,5 @@ from .main_window import Ui_mainWindow
 from .status_form import Ui_statusForm
 from .settings_form import Ui_settingsForm
 from .scan_form import Ui_scanForm
+from .history_form import Ui_historyForm
+from .history_item import Ui_historyItem
