553fe8b6e37c1aba6eec23923407ec48acc11611
Helanka Singhapurage <helankas26@gmail.com>
2024-01-09 07:22:08 +0530

    add system tray

--- src/guardiannet/app/main.py
+++ src/guardiannet/app/main.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 
 from src.guardiannet.app.controller import Login, Signup
 from src.guardiannet.app.util import MongoDBManager, WindowManager, ConfigurationManager
@@ -13,7 +14,24 @@ def loadWindow():
         return Signup()
 
 
+def onTrayIconActivated(reason):
+    if reason == QSystemTrayIcon.ActivationReason.Trigger:
+        WindowManager.window.showNormal()
+        WindowManager.window.activateWindow()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    app.setQuitOnLastWindowClosed(False)
+    tray = QSystemTrayIcon(QIcon("assert/logo.png"))
+    menu = QMenu()
+    exitApp = QAction("EXIT")
+    exitApp.triggered.connect(app.quit)
+    menu.addAction(exitApp)
+    tray.setContextMenu(menu)
+    tray.activated.connect(onTrayIconActivated)
+    tray.show()
+
     WindowManager.window = loadWindow()
     sys.exit(app.exec())
