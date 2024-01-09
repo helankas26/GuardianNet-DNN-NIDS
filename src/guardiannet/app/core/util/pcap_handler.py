76d7eeb076098e7e52665091af268ec2d962a579
Helanka Singhapurage <helankas26@gmail.com>
2024-01-09 21:06:17 +0530

    path change

--- src/guardiannet/app/core/util/pcap_handler.py
+++ src/guardiannet/app/core/util/pcap_handler.py
@@ -6,17 +6,17 @@ from scapy.utils import wrpcap, rdpcap
 class PcapHandler:
     @staticmethod
     def savePackets(packets, filename):
-        filepath = f"../../../temp/pcap/{filename}.pcap"
+        filepath = f"../../../../temp/pcap/{filename}.pcap"
         wrpcap(filepath, packets)
 
     @staticmethod
     def loadPackets(filename):
-        filepath = f"../../../temp/pcap/{filename}.pcap"
+        filepath = f"../../../../temp/pcap/{filename}.pcap"
         return rdpcap(filepath)
 
     @staticmethod
     def deletePackets(filename):
-        filepath = f"../../../temp/pcap/{filename}.pcap"
+        filepath = f"../../../../temp/pcap/{filename}.pcap"
         if os.path.exists(filepath):
             try:
                 os.remove(filepath)
